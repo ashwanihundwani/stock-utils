ae2c97452c73009d15b6dafd8e8ca53b42d84d06
Ashwani Hundwani <ashwin.hundwani456@gmail.com>
2025-03-12 21:09:27 +0530

    Create SVG.ts

---
+++ SVG.ts
@@ -0,0 +1,25 @@
+export function BackArrow() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M4.1665 10L16.6665 9.9998"
+        stroke="#7D7495"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M7.49967 5.83398L4.04011 9.29355C3.70678 9.62688 3.54011 9.79354 3.54011 10.0007C3.54011 10.2078 3.70678 10.3744 4.04011 10.7078L7.49967 14.1673"
+        stroke="#7D7495"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
